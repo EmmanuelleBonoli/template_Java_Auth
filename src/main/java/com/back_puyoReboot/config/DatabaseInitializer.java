9842317929273506c454b0a25d0804e7098c959a
Emmanuelle B <128315974+EmmanuelleBonoli@users.noreply.github.com>
2025-05-21 19:44:21 +0200

    Feature/issue 1/add prettier (#1)
    * add server port config
    
    * add prettier
    
    ---------
    
    Co-authored-by: Emmanuelle BONOLI <manu.bonoli@gmail.com>

--- src/main/java/com/back_puyoReboot/config/DatabaseInitializer.java
+++ src/main/java/com/back_puyoReboot/config/DatabaseInitializer.java
@@ -1,13 +1,12 @@
 package com.back_puyoReboot.config;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-
 import com.back_puyoReboot.User.AccountEnumType;
 import com.back_puyoReboot.User.User;
 import com.back_puyoReboot.User.UserEnumType;
 import com.back_puyoReboot.User.UserRepository;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,33 +14,41 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DatabaseInitializer {
 
-    private final UserRepository userRepository;
+  private final Long PLAYER_NUMBER_ONE = 1L;
+  private final Long PLAYER_NUMBER_TWO = 2L;
 
-    public DatabaseInitializer(
-            UserRepository userRepository
-    ) {
-        this.userRepository = userRepository;
-    }
+  private final UserRepository userRepository;
 
-    @Bean
-    CommandLineRunner init() {
+  public DatabaseInitializer(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
 
-        List<User> users = Arrays.asList(
-                new User(
-                        new HashSet<>(List.of(UserEnumType.ROLE_PLAYER, UserEnumType.ROLE_ADMIN)),
-                        AccountEnumType.ACTIVE,
-                        "$2a$10$jw6BeI/txUaC1BQNGYZn4.hs5wpmLhe2uYpTBB40oUveFE3ZRQYQq", // password is "Password"
-                        "manu@gmail.com",
-                        "MajorManu",
-                        "/images/User/DonkeyKong.png",
-                        1L
-                )
-        );
+  @Bean
+  CommandLineRunner init() {
+    List<User> users = Arrays.asList(
+      new User(
+        new HashSet<>(List.of(UserEnumType.ROLE_PLAYER, UserEnumType.ROLE_ADMIN)),
+        AccountEnumType.ACTIVE,
+        "$2a$10$jw6BeI/txUaC1BQNGYZn4.hs5wpmLhe2uYpTBB40oUveFE3ZRQYQq", // password is "Password"
+        "manu@gmail.com",
+        "MajorManu",
+        "/images/User/DonkeyKong.png",
+        PLAYER_NUMBER_ONE
+      ),
+      new User(
+        new HashSet<>(List.of(UserEnumType.ROLE_PLAYER)),
+        AccountEnumType.ACTIVE,
+        "$2a$10$jw6BeI/txUaC1BQNGYZn4.hs5wpmLhe2uYpTBB40oUveFE3ZRQYQq", // password is "Password"
+        "player@gmail.com",
+        "Player1",
+        "/images/User/DonkeyKong.png",
+        PLAYER_NUMBER_TWO
+      )
+    );
 
-        return args -> {
-            // save general data
-            userRepository.saveAll(users);
-
-        };
-    }
+    return args -> {
+      // save general data
+      userRepository.saveAll(users);
+    };
+  }
 }
